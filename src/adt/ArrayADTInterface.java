8d52fba483727f48f9f8cbf4c21afc59ca8cee9e
divaibhav <divaibhav@gmail.com>
2020-01-25 03:08:49 +0530

    declared all the operations

--- src/adt/ArrayADTInterface.java
+++ src/adt/ArrayADTInterface.java
@@ -7,5 +7,17 @@
 
 package adt;
 
-public class ArrayADTInterface {
+public interface ArrayADTInterface {
+    //insert at next empty index, defined by end
+    boolean insert(int element);
+    //delete the last inserted element and return that element
+    int delete();
+    // to print elements of array, till end index
+    void traverse();
+    // search the given element
+    boolean search(int element);
+    // sort the existing array and preserve the original
+    int[] sort();
+    // checks for empty array
+    boolean isEmpty();
 }
